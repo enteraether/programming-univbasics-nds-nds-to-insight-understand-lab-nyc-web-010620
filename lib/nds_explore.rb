98dfa63d475d490ef22ce657e312d16f5ab6aadc
Theresa <thetheresagarcia@gmail.com>
2020-01-02 17:50:19 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
 require 'pry'
-# binding.pry
+binding.pry
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
